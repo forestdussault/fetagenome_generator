e5a313057dd0c19673d6a0c11f2da04ee838f8aa
forestdussault <forest.dussault@inspection.gc.ca>
2018-03-22 15:43:07 -0400

    Refactored + single-ended support

--- srst2_wrapper.py
+++ srst2_wrapper.py
@@ -3,7 +3,7 @@ import glob
 import click
 import pandas as pd
 from subprocess import Popen
-from fetagenome import find_paired_reads
+from accessories import find_paired_reads
 
 
 def run_srst2(sampleid, r1, r2, gene_db, output_directory, threads=10):
@@ -17,8 +17,7 @@ def run_srst2(sampleid, r1, r2, gene_db, output_directory, threads=10):
     :param threads: Number of CPUs to dedicate to task
     """
     print('\nRunning SRST2 on {}'.format(sampleid))
-    srst2_output = os.path.join(output_directory,
-                                sampleid + '_SRST2')
+    srst2_output = os.path.join(output_directory, sampleid + '_SRST2')
     cmd = 'srst2 ' \
           '--input_pe {r1} {r2} ' \
           '--gene_db {gene_db} ' \
@@ -27,7 +26,24 @@ def run_srst2(sampleid, r1, r2, gene_db, output_directory, threads=10):
           ''.format(r1=r1, r2=r2, gene_db=gene_db, output=srst2_output, threads=threads)
     p = Popen(cmd, shell=True)
     p.wait()
+    srst2_output_cleanup(output_directory)
 
+
+def run_srst2_se(sampleid, fastq, gene_db, output_directory, threads=10):
+    print('\nRunning SRST2 on {}'.format(sampleid))
+    srst2_output = os.path.join(output_directory, sampleid + '_SRST2')
+    cmd = 'srst2 ' \
+          '--input_se {fastq} ' \
+          '--gene_db {gene_db} ' \
+          '--output {output} ' \
+          '--threads {threads} ' \
+          ''.format(fastq=fastq, gene_db=gene_db, output=srst2_output, threads=threads)
+    p = Popen(cmd, shell=True)
+    p.wait()
+    srst2_output_cleanup(output_directory)
+
+
+def srst2_output_cleanup(output_directory):
     # Remove extra junk
     txt_files = glob.glob(os.path.join(output_directory, '*.txt'))
     txt_files = [x for x in txt_files if 'fullgenes' in x]
@@ -66,17 +82,31 @@ def combine_results(srst2_result_directory):
               type=click.Path(exists=True),
               required=True,
               help='Gene targets to search against')
-def main(input_directory, output_directory, gene_db):
-    # Get pairs
-    pair_dict = find_paired_reads(input_directory)
-
-    # Run on every sample
-    for sampleid, reads in pair_dict.items():
-        run_srst2(sampleid=sampleid,
-                  r1=reads[0],
-                  r2=reads[1],
-                  gene_db=gene_db,
-                  output_directory=output_directory)
+@click.option('-se', '--single_ended',
+              required=False,
+              is_flag=True,
+              default=False,
+              help='Set this flag if your folder contains single ended reads')
+def main(input_directory, output_directory, gene_db, single_ended):
+    if single_ended:
+        reads = glob.glob(os.path.join(input_directory, '*.f*q*'))
+        for read in reads:
+            # Run on every sample
+            sampleid = os.path.basename(read)
+            run_srst2_se(sampleid=sampleid,
+                         fastq=read,
+                         gene_db=gene_db,
+                         output_directory=output_directory)
+    else:
+        # Get pairs
+        pair_dict = find_paired_reads(input_directory)
+        # Run on every sample
+        for sampleid, reads in pair_dict.items():
+            run_srst2(sampleid=sampleid,
+                      r1=reads[0],
+                      r2=reads[1],
+                      gene_db=gene_db,
+                      output_directory=output_directory)
 
     # Create final output file
     combine_results(srst2_result_directory=output_directory)
